87dd0af8459a79dc9451585859e2c67de54a8eeb
juyu <yujeongmin@juyu-MacBook-Pro.local>
2023-11-05 18:00:32 +0900

    Initial commit

---
+++ src/chapter1_10/MyClass2.java
@@ -0,0 +1,20 @@
+package chapter1_10;
+
+/**
+ * CopyRight 작성자: 유재빈
+ * Author
+ */
+
+public class MyClass2 {
+    public static void main(String[] args){
+        // 이것은 라인 주석 입니다.
+        // 이것은 라인 주석 2 입니다.
+        // Hello World를 출력하는 구문입니다.
+        // System.out.println("Hello world2");
+        // System.out.println("Bye World");
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
