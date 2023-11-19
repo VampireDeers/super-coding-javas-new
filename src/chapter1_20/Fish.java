66b6cab709c0ea1ad748d226f574bba7ff89b69c
VampireDeers <VampireDeers>
2023-11-19 18:27:10 +0900

    feat: 수업 코드 추가

---
+++ src/chapter1_20/Fish.java
@@ -0,0 +1,14 @@
+package chapter1_20;
+
+public class Fish {
+    // 속성
+    String sex; // "Male", "Female"
+    boolean havingPoison; // true 독이있고, false: 독이 없고
+    String spawningDate; // 산란
+    String leavingSea; // 사는 속성
+    // 행위
+    void eat(String food){}
+    void swim(int meter){}
+    void makeCrowd(Fish otherfish){}
+    void sleep(){};
+}
